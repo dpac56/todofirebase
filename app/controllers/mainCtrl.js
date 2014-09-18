0ccc8a6e58dd7a78ff0b9d5f57b8793bf431657e
deepak <deepak@withstartups.com>
2014-09-18 10:31:47 +0530

    Final version for base features with no backend

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -3,30 +3,34 @@
   //variables initiated not in controller
   var oneWeekInEpoch = 604800000;
   
-  function TodoItem(item, date) {
+  function TodoItem(item, date, timeLimit) {
     this.item = item;
     this.date = date;
+    this.timeLimit = timeLimit;
     this.daysLeft = function () {
       return (7-(Date.now() - this.date)/(1000*24*60*60));
+    };
+    this.isCurrent = function(){
+      return Date.now() - this.date < this.timeLimit;
     };
   }
 
   //controller starts
   var mainCtrl = function($scope){
     $scope.todos = [
-      new TodoItem("Item 1", 1310763878804),
-      new TodoItem("Item 2", 1410763878804),
+      new TodoItem("Item 1", 1310763878804, oneWeekInEpoch),
+      new TodoItem("Item 2", 1410763878804, oneWeekInEpoch),
       //Another way to add items
       //{item: "Item 3", date: 1410763878804, daysLeft: function () { return (7-(Date.now() - this.date)/(1000*24*60*60))}},
       ];
 
-    $scope.dateNow = Date.now();
+    //$scope.dateNow = Date.now();
 
     $scope.addTodo = function(){
 
         var dateWhenItemAdded = Date.now();
 
-        $scope.todos.push(new TodoItem($scope.newItem, dateWhenItemAdded));
+        $scope.todos.push(new TodoItem($scope.newItem, dateWhenItemAdded, oneWeekInEpoch));
 
         $scope.newItem = '';
       };
@@ -35,17 +39,7 @@
         $scope.todos.splice($index, 1);
       };
 
-    $scope.itemStatus = function($index){
-        if ($scope.dateNow - $scope.todos[$index].date < oneWeekInEpoch){
-          return true;
-        } else {
-          return false;
-        }
-      };
-
     };
-
-
 
   mainCtrl.$inject = ['$scope'];
 
